81ec28e8abe01797280a7066a73e46804712981b
namiqmamedov <namigmm@code.edu.az>
2023-07-24 00:58:58 +0400

    Updated components,lib & graphql

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -24,7 +24,6 @@ const AuthProviders = () => {
 
       console.log(res);
       
-
       setProviders(res);
     }
 
--- lib/session.ts
+++ lib/session.ts
@@ -4,6 +4,8 @@ import {AdapterUser} from 'next-auth/adapters'
 import GoogleProvider from 'next-auth/providers/google'
 import jsonwebtoken from 'jsonwebtoken'
 import {JWT} from 'next-auth/jwt'
+import { SessionInterface, UserProfile } from '@/common.types'
+import { createUser, getUser } from './actions'
 
 type AdapterUser = typeof AdapterUser
     
@@ -28,13 +30,36 @@ export const authOptions: NextAuthOptions = {
     },
     callbacks: {
         async session({session}) {
-            return session;
+            const email = session?.user?.email as string;
+
+            try {
+                const data = await getUser(email) as {user? :UserProfile}
+
+                const newSession = {
+                    ...session,
+                    user: {
+                        ...session.user,
+                        ...data?.user
+                    }
+                }
+
+                return newSession;
+            } catch (error) {
+                console.log('Error retrieving user data',error);
+                
+                return session;
+            }
         },
         async signIn({user} : {user: AdapterUser | User}) {
             try {
-                //get user if they exist
+               const userExists = await getUser(user?.email as string) as {user?: UserProfile}
 
-                // if they don't exist, create them
+               if(!userExists.user) {
+                await createUser(
+                    user.name as string,
+                    user.email as string,
+                    user.image as string);
+               }
 
                 return true;                
             } catch (error) {
@@ -43,4 +68,10 @@ export const authOptions: NextAuthOptions = {
             }
         }
     }
+}
+
+export async function getCurrentUser() {
+    const session = await getServerSession(authOptions) as SessionInterface;
+
+    return session;
 }
