4b0b4eb7c8f8fe6cae8f8630a1c6d2e17bbf7bbd
namiqmamedov <namigmm@code.edu.az>
2023-07-27 13:22:26 +0400

    Updated components & lib

---
+++ components/ProjectCard.tsx
@@ -0,0 +1,19 @@
+
+type Props = {
+    id: string;
+    image: string;
+    title: string;
+    name: string;
+    avatarUrl: string;
+    userId: string; 
+}
+
+const ProjectCard = ({id,image,title,name,avatarUrl,userId} : Props) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ProjectCard
